c0eb858758908e47874fd6a64cf3403933ca317c
Arpitthedamager <sg79720@gmail.com>
2024-11-26 13:00:18 +0530

    none

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default async function Home() {
 
   return (
     <>
-    <div className="min-h-screen bg-primary-content">
-    <ThemeButton/>
-
-      <p className="text-primary">hi i am a devloper</p>
-    </div>
+      <div className="min-h-screen bg-primary-content">
+        <ThemeButton />
+        <p className="text-primary">hi i am a devloper</p>
+        <a href="/notes" >link</a>
+      </div>
     </>
   );
 }
