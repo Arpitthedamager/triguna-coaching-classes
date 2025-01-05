5fc4043ad5bf31c27f72830eb770f535ea7f3711
Arpitthedamager <sg79720@gmail.com>
2025-01-05 14:37:47 +0530

    feat: add next-sitemap integration for improved SEO and generate sitemap and robots.txt files

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://trigunacoachingclasses.in',
+    generateRobotsTxt: true, // Generate robots.txt file as well
+    changefreq: 'daily', // Frequency of updates
+    priority: 0.7, // Default priority for all pages
+    sitemapSize: 5000, // Limit number of URLs per sitemap file
+  };
+  
